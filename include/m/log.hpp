45e993f1545ab4c60d1a77b0d3f1dd6a3e6ebdd0
Andrew Mass <amass1212@gmail.com>
2023-08-27 09:06:09 -0500

    log: Add support for message callbacks.
    Also add missing cout flush.

--- include/m/log.hpp
+++ include/m/log.hpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
@@ -21,8 +22,14 @@ class log
 
     enum Severity { INFO, WARN, ERROR };
 
+    using Callback_t = std::function<void(Severity,std::string_view)>;
+
     static inline void init_file(std::string_view file) {
       _file = std::ofstream(std::string(file), std::ofstream::out | std::ofstream::app);
+    }
+
+    static inline void init_callback(Callback_t&& callback) {
+      _callback = std::move(callback);
     }
 
     log(Severity s);
@@ -45,13 +52,17 @@ class log
     log& operator=(const log& o) = delete;
     log& operator=(log&& o) = delete;
 
-    static inline std::ofstream _file;
+    static inline std::ofstream _file = {};
+    static inline Callback_t _callback = {};
+
+    Severity _s;
     std::stringstream _stream;
 };
 
 /// Implementation ///////////////////////////////////////////////////////////
 
 inline log::log(Severity s)
+  : _s(s)
 {
   static constexpr std::array<const char*, 3> SEVERITY_STRING = { "I", "W", "E" };
 
@@ -70,10 +81,15 @@ inline log::~log()
   const auto str = _stream.str();
 
   std::cout << str;
+  std::cout.flush();
 
   if (_file) {
     _file << str;
     _file.flush();
+  }
+
+  if (_callback) {
+    _callback(_s, str);
   }
 }
 
